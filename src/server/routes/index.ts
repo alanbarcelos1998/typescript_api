6073a6e97e1d4d1a01053b1a4cadf97eb1cf4293
alanbarcelos1998 <alanbasiliobarcelos@hotmail.com>
2023-03-08 00:12:02 -0300

    Novas regras, controllers e models.

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,16 +1,15 @@
 import { Router } from 'express';
 import {StatusCodes} from 'http-status-codes';
 
+import { CidadesController } from '../controllers/';
+
+
 const router = Router();
 
 router.get('/', (req, res) => {
     return res.send('Olá world!');
 });
 
-router.post('/teste', (req, res) => {
-    console.log(req);
-    
-    return res.status(StatusCodes.ACCEPTED).json(req.body);
-});
+router.post('/cidades', CidadesController.create);
 
 export { router };
